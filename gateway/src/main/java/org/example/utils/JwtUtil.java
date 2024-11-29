52375b5ed7cd942ae4a2c60ad12334f818d82af3
liuyunbaichuan <860523922@qq.com>
2024-11-29 16:18:35 +0800

    功能实现

--- gateway/src/main/java/org/example/utils/JwtUtil.java
+++ gateway/src/main/java/org/example/utils/JwtUtil.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import java.util.Calendar;
 import java.util.Map;
 
-@Component
 public class JwtUtil {
     private static final String SING = "XIAOSHUANG";
 
